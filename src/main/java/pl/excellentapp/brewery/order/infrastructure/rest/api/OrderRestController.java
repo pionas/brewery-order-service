a02fb562729b5f058b634602c7ec3d89487caf1d
Adrian Pionka <adrian@pionka.com>
2025-01-26 10:30:16 +0100

    add GlobalExceptionHandler

--- src/main/java/pl/excellentapp/brewery/order/infrastructure/rest/api/OrderRestController.java
+++ src/main/java/pl/excellentapp/brewery/order/infrastructure/rest/api/OrderRestController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,15 +13,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.excellentapp.brewery.order.application.OrderService;
-import pl.excellentapp.brewery.order.domain.exception.OrderNotFoundException;
 import pl.excellentapp.brewery.order.infrastructure.rest.api.dto.OrderRequest;
 import pl.excellentapp.brewery.order.infrastructure.rest.api.dto.OrderResponse;
 import pl.excellentapp.brewery.order.infrastructure.rest.api.dto.OrdersResponse;
 import pl.excellentapp.brewery.order.infrastructure.rest.api.mapper.OrderRestMapper;
 
-import java.util.Date;
-import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -70,19 +65,6 @@ class OrderRestController {
     public ResponseEntity<HttpStatus> deleteOrder(@PathVariable("orderId") UUID orderId) {
         orderService.delete(orderId);
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
-    }
-
-    @ExceptionHandler(OrderNotFoundException.class)
-    public ResponseEntity<Object> handle(OrderNotFoundException ex) {
-        return handleError(HttpStatus.NOT_FOUND, List.of(ex.getMessage()));
-    }
-
-    private ResponseEntity<Object> handleError(HttpStatus status, List<String> errors) {
-        final var body = new LinkedHashMap<String, Object>();
-        body.put("timestamp", new Date());
-        body.put("status", status.value());
-        body.put("errors", errors);
-        return new ResponseEntity<>(body, status);
     }
 
 }
---
+++ src/main/java/pl/excellentapp/brewery/order/infrastructure/rest/handler/GlobalExceptionHandler.java
@@ -0,0 +1,61 @@
+package pl.excellentapp.brewery.order.infrastructure.rest.handler;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import pl.excellentapp.brewery.order.domain.exception.OrderNotFoundException;
+import pl.excellentapp.brewery.order.utils.DateTimeProvider;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+@Slf4j
+@Component
+@AllArgsConstructor
+public class GlobalExceptionHandler {
+
+    private DateTimeProvider dateTimeProvider;
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    public ResponseEntity<Object> handle(OrderNotFoundException ex) {
+        log.warn("Not found: {}", ex.getMessage());
+        return handleError(HttpStatus.NOT_FOUND, List.of(ex.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handle(Exception ex) {
+        log.error("An unexpected error occurred", ex);
+        return handleError(HttpStatus.INTERNAL_SERVER_ERROR, List.of(ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        log.warn("Validation error: {}", ex.getMessage());
+        Map<String, List<String>> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .collect(Collectors.groupingBy(
+                        FieldError::getField,
+                        Collectors.mapping(FieldError::getDefaultMessage, Collectors.toList())
+                ));
+
+        return handleError(HttpStatus.BAD_REQUEST, errors);
+    }
+
+    private ResponseEntity<Object> handleError(HttpStatus status, Object errors) {
+        final var body = new LinkedHashMap<String, Object>();
+        body.put("timestamp", dateTimeProvider.now());
+        body.put("status", status.value());
+        body.put("errors", errors);
+        return new ResponseEntity<>(body, status);
+    }
+}
