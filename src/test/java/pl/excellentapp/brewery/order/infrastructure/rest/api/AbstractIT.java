11a475362641b2b08a8abaeb2e4262120dd6c58c
pionas <pionas@wsieci.ws>
2025-02-03 20:03:24 +0100

    add wiremock configuration

--- src/test/java/pl/excellentapp/brewery/order/infrastructure/rest/api/AbstractIT.java
+++ src/test/java/pl/excellentapp/brewery/order/infrastructure/rest/api/AbstractIT.java
@@ -4,11 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @Import({RestTemplateConfiguration.class})
-@Profile("it")
+@ActiveProfiles("it")
 public abstract class AbstractIT {
 
     @Autowired
